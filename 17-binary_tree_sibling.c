6e37354da8689e2ae40eb08bbc410f0caa8a51c3
Arnaud875 <171556021+Arnaud875@users.noreply.github.com>
2024-08-06 13:38:26 +0200

    Update task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,7 +9,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	return ((node->parent->right && node->parent->right->n != node->n)
-				? node->parent->right
-				: node->parent->left);
+	return (node->parent->left == node ? node->parent->right : node->parent->left);
 }
